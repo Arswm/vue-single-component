e19133add1f9cf07d67964d0b69efc797cee0cdc
Arswm <98147604+Arswm@users.noreply.github.com>
2023-06-16 23:43:54 +0330

    vue is fun

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const app = Vue.createApp({
+
+  const app = Vue.createApp({
     data() {
-      return {
-        showBooks: true,
-        title: 'the final empire',
-        author: 'arsawm',
-        age: 23
-      }
+        return {
+            showBooks: true,
+            title: 'the final empire',
+            author: 'arsawm',
+            age: 23,
+            x: 0,
+            y: 0
+        }
     },
-    methods: { // <--- fix the typo here
-      toggleShowBooks() {
-        this.showBooks = !this.showBooks;
-      }
+    methods: {
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks;
+        },
+        handleEvent(event , data) {
+            console.log(event , event.type);
+            if (data) {
+                console.log(data);
+            }
+        },
+        handleMouseMove(e) {
+            this.x=e.offsetX
+            this.y=e.offsetY
+        } 
     }
-  })
-  app.mount('#app')
+})
+app.mount('#app')
